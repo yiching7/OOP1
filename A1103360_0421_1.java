abbf4c26e61a9723227ec82ffc072f1fda3ce866
yiching7 <125584577+yiching7@users.noreply.github.com>
2023-04-28 02:22:20 +0800

    Add files via upload

---
+++ A1103360_0421_1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class A1103360_0421_1{
+    public static void main(String [] args) throws Exception{
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("請輸入電子郵件信箱:");
+        String mail = input.next();
+
+        if(mail.matches("[a-z]+[@]{1}[a-z]+")){
+            System.out.println(mail);
+        }
+        else{
+            System.out.println("電子郵件信箱格式不正確");
+        }
+    }
+}
